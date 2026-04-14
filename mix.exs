fec3428cc9c3f15d23dffffebcb34abc66681adc
Josh Rotenberg <joshrotenberg@gmail.com>
2026-04-14 14:00:08 -0700

    chore: bump to 0.8.0, regenerate with tinfoil 0.2.18 (attestations + ps1)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TinfoilDemo.MixProject do
   def project do
     [
       app: :tinfoil_demo,
-      version: "0.7.2",
+      version: "0.8.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
